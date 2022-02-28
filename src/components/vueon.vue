0b78d51f2a68ba18cbc15c8122e112c6c29997f4
Ayusummer <1369661643@qq.com>
2022-02-28 16:38:02 +0800

    v-on语法认识

---
+++ src/components/vueon.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+const fatherClick = (): void => {
+    console.log("father 被点击了")
+}
+
+const childClick = (): void => {
+    console.log("child 被点击了")
+}
+
+</script>
+
+<template>
+    <div class="father" @click.self="fatherClick">
+        <div class="child" @click="childClick"></div>
+    </div>
+</template>
+
+<style scoped>
+.father {
+    height: 300px;
+    width: 300px;
+    background: pink;
+}
+
+.child {
+    width: 200px;
+    height: 200px;
+    background: green;
+}
+</style>
