291acad9b1e961c694018eca497f31ebb31dc51d
Ayusummer <1369661643@qq.com>
2022-02-28 16:38:23 +0800

    v-model实现简易计算器

---
+++ src/components/vuemodel.vue
@@ -0,0 +1,46 @@
+<!-- v-model 实现简易计算器 -->
+<script setup lang="ts">
+import { ref, Ref } from 'vue'
+
+const n1: Ref<number> = ref(0)
+const n2: Ref<number> = ref(0)
+const result: Ref<number> = ref(0)
+const opt: Ref<string> = ref('+')
+
+const calculate = (): void => {
+    switch (opt.value) {
+        case '+':
+            result.value = n1.value + n2.value
+            break
+        case '-':
+            result.value = n1.value - n2.value
+            break
+        case '*':
+            result.value = n1.value * n2.value
+            break
+        case '/':
+            result.value = n1.value / n2.value
+            break
+        default:
+            break
+    }
+}
+
+</script>
+
+<template>
+    {{ result }}
+    <input type="number" v-model="n1" />
+    <select v-model="opt">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+    </select>
+    <input type="number" v-model="n2" />
+    <input type="button" value="=" @click="calculate" />
+    <input type="number" v-model="result" />
+</template>
+
+<style scoped>
+</style> 
