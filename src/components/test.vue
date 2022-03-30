ce28f35a211f8d7d4f6408274b4b6132d07ff33b
Ayusummer <1369661643@qq.com>
2022-03-30 22:45:50 +0800

    加个测试组件

---
+++ src/components/test.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { defineComponent, defineProps, inject, ref, Ref } from 'vue'
+let flag = ref(false)
+</script>
+
+<template>
+    <div></div>
+</template>
+
+<style lang="less" scoped>
+</style>
