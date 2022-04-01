8ae932ad8a7434349e109c70ce6e290c1191ab60
Ayusummer <1369661643@qq.com>
2022-04-01 20:00:26 +0800

    delete path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-const path = require('path')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    // 配置路径别名
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+  plugins: [vue()]
 })
