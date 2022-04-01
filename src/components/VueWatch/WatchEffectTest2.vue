5ab37834b140270f9da4a80cb0bdcbef1737b601
Ayusummer <1369661643@qq.com>
2022-04-01 20:02:04 +0800

    WatchEffect hooks test

---
+++ src/components/VueWatch/WatchEffectTest2.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+
+const counter = ref(0)
+const enabled = ref(false)
+
+watchEffect(() => {
+    if (enabled.value)
+        console.log(counter.value)
+    else
+        console.log("disabled")
+})
+
+</script>
+
+<template>
+    <div>
+        {{ counter }}
+        <button @click="counter += 1">counter + 1</button>
+    </div>
+    <div>
+        {{ enabled }}
+        <button @click="enabled = !enabled">enabled change</button>
+    </div>
+</template>
+
+<style lang="less" scoped>
+</style>
