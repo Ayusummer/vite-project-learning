9a786552a5ff74d049a46bdbdad2d0c5d3fa3fd5
Ayusummer <1369661643@qq.com>
2022-04-02 21:39:05 +0800

    scoped 内使用 :deep 修改 ElementPlus 组件样式

---
+++ src/components/ScopedDeepTest/ELementUIInputStyleChange.vue
@@ -0,0 +1,21 @@
+<!-- 创建时间: 2022/4/2-21-04-29 -->
+<!-- 路径: src/components/ScopedDeepTest -->
+<!-- IDE: WebStorm -->
+<!-- 创建者: 233 -->
+<template>
+<div style="margin:200px;">
+  <el-input class="ipt"></el-input>
+</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="less">
+.ipt{
+  // 样式穿透修改 input 样式
+  :deep(input){
+    background-color:red;
+  }
+}
+</style>
