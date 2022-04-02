2621ed8e6518f044b782056710e90cd11ed6fd7b
Ayusummer <1369661643@qq.com>
2022-04-02 17:17:30 +0800

    refatcor

--- src/components/Loading/loading.vue
+++ src/components/Loading/loading.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
-import {axios} from './server'
+import { axios } from './server'
 
 const list = await axios('./data.json')
 console.log(list)
 </script>
 
 <template>
-    <div v-for="item in list"> 
-        {{item.name}}
-    </div>
+    <div v-for="item in list">{{ item.name }}</div>
 </template>
 
 <style lang="less" scoped>
