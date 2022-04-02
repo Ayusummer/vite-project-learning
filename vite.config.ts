224b2986171f0ab23c952dc52dd31b5690bc17f7
Ayusummer <1369661643@qq.com>
2022-04-02 19:54:30 +0800

    路径别名定义

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { join } from "path";
+
+const {resolve} = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': join(__dirname, "src"),
+    }
+  }
 })
