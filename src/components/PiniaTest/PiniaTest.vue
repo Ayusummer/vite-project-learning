15243f65998c96429cd1f86d16a0a1dc816921e7
Ayusummer <1369661643@qq.com>
2022-04-04 09:16:04 +0800

    Change Pinia State

--- src/components/PiniaTest/PiniaTest.vue
+++ src/components/PiniaTest/PiniaTest.vue
@@ -2,10 +2,45 @@
 import { useTestStore } from '@/store';
 
 const useTest = useTestStore()
+
+// 直接修改属性值实现 useTest.current++
+const useTestChange1 = () => {
+  useTest.current++
+}
+// 方法2: 通过$patch 批量修改属性值
+const useTestChange2 = () => {
+  useTest.$patch({
+    name: '马克杯',
+    current: 10
+  })
+}
+// 方法3: $patch 函数式写法
+const useTestChange3 = () => {
+  useTest.$patch( (state) => {
+    state.name = '立牌'
+    state.current = 5
+  })
+}
+// 方法4: 通过原始对象修改整个实例(缺点在于需要修改state所有属性, 因此一般不建议使用)
+const useTestChange4 = () => {
+  useTest.$state = {
+    name: '小夜灯',
+    current: 7
+  }
+}
+// 方法5: 通过 actions 修改
+const useTestChange5 = () => {
+  useTest.currentIncrement()
+}
 </script>
 
 <template>
-    <div>pinia:{{ useTest.current }} -- {{ useTest.name }}</div>
+    <div>pinia: {{ useTest.name }} -- ${{ useTest.current }}</div>
+    <button @click="useTestChange1">increment-直接修改属性值</button>
+    <button @click="useTestChange2">通过$patch批量修改属性</button>
+    <button @click="useTestChange3">$patch的函数式写法</button>
+    <button @click="useTestChange4">通过原始对象修改整个实例</button>
+    <button @click="useTestChange5">通过 actions 修改</button>
 </template>
 
 <style lang="less" scoped>
--- src/store/index.ts
+++ src/store/index.ts
@@ -6,7 +6,7 @@ export const useTestStore = defineStore(Names.TEST, {
     state: () => {
         return {
             current: 1,
-            name:'233'
+            name:'Cola'
         }
     },
     // computed like, 修饰一些值, 用于监视(计算)状态变化, 有缓存的功能
@@ -15,6 +15,9 @@ export const useTestStore = defineStore(Names.TEST, {
     },
     // methods, 可做同步异步, 提交state(用于修改 state 全局状态数据)
     actions: {
-        
+        // current++
+        currentIncrement() {
+            this.current++
+        },
     }
 })
