9dab7bf998e6fbe90226fee2255a2a33d09d3141
Ayusummer <1369661643@qq.com>
2022-04-07 11:03:11 +0800

    Pinia 持久化插件

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,48 @@
-import { createApp } from 'vue'
+import { createApp, toRaw } from 'vue'
 import App from './App.vue'
 import './assets/css/reset.less'
 import Card from './components/Card.vue'
 import AnotherLoading from './components/AnotherLoading/AnotherLoading'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import { createPinia } from 'pinia'
+import { createPinia, PiniaPluginContext } from 'pinia'
+
+
+type Options = {
+    key?:string
+}
+// 默认配置
+const __piniaKey__ = 'yusummer'
+
+// 将 key 存入 localstorage
+const setStorage = (key: string, value: any) => {
+    localStorage.setItem(key, JSON.stringify(value))
+}
+
+// 根据 key 从 localstorage 获取数据
+const getStorage = (key: string) => {
+    return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key) as string) : null
+}
+
+// 定义 pinia 插件
+const piniaPlugin = (options:Options) => {
+    return (context: PiniaPluginContext) => {
+        const { store } = context;
+        // 从 localstorage 获取数据
+        const data = getStorage(`${options.key ?? __piniaKey__}-${store.$id}`) 
+        console.log(data)
+        // state 有变化时, 将数据存入 localstorage
+        store.$subscribe(() => {
+            setStorage(`${options.key ?? __piniaKey__}-${store.$id}`, toRaw(store.$state))
+        })
+        
+        console.log("store", store)
+
+        return {
+            ...data
+        }
+    }
+}
 
 // export const app = createApp(App)
 const app = createApp(App)
@@ -14,6 +51,9 @@ app.use(ElementPlus)
 
 // 引入 pinia
 const store = createPinia()
+store.use(piniaPlugin({
+    key: 'pinia'
+}))
 app.use(store)
 
 // 定义 Filter 类型, 作为 $filters 的返回类型
