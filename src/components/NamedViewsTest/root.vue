0be2e548dc6cc72e2b6c7aa4504dc2348a13730f
Ayusummer <ayusummer233@qq.com>
2022-04-16 17:34:42 +0800

    改一改 namedVIew navigationbar

--- src/components/NamedViewsTest/root.vue
+++ src/components/NamedViewsTest/root.vue
@@ -4,6 +4,8 @@
 <template>
     <!-- 返回主界面 -->
     <el-button @click="$router.push('/')">返回主界面</el-button>
+    <!-- 跳转到 namedView主界面 -->
+    <el-button @click="$router.push({ name: 'namedView' })">跳转到 namedView 主界面</el-button>
     <!-- 跳转到 user1 -->
     <el-button @click="$router.push('/namedView/user1')">跳转到 user1</el-button>
     <!-- 跳转到 user2 -->
